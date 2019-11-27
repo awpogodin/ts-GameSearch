1df12eb0aa5712611438b63557a436f51e6f4cdf
Alexandr Pogodin <awpogodin@mail.ru>
2019-11-27 22:37:40 +0400

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import "./styles/index.css";
+/* eslint-disable no-console */
+import './styles/index.css';
 
-const searchInput: HTMLInputElement = document.querySelector(".js-search-input");
-const clearBtn: HTMLElement = document.querySelector(".js-clear-btn");
+const searchInput: HTMLInputElement = document.querySelector('.js-search-input');
+const clearBtn: HTMLElement = document.querySelector('.js-clear-btn');
 
-clearBtn.addEventListener("click", (e: Event): void => {
+clearBtn.addEventListener('click', (e: Event): void => {
   e.preventDefault();
   console.log(searchInput);
-  searchInput.value = "";
+  searchInput.value = '';
 });
