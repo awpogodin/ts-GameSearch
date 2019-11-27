2b9405f213d8045a4b48da6bed6071e6aba45ad3
Alexandr Pogodin <awpogodin@mail.ru>
2019-11-27 18:50:58 +0400

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import "./styles/index.css";
 
-const searchInput: HTMLInputElement = document.querySelector(
-  ".js-search-input"
-);
+const searchInput: HTMLInputElement = document.querySelector(".js-search-input");
 const clearBtn: HTMLElement = document.querySelector(".js-clear-btn");
 
 clearBtn.addEventListener("click", (e: Event): void => {
